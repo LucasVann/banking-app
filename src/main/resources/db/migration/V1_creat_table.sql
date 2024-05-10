d796551044e0fec82d830afa96fcec0c30985bda
Lucas <Vannucchilucas8@gmail.com>
2024-05-10 16:08:05 +0200

    Arreglo de query

--- src/main/resources/db/migration/V1_creat_table.sql
+++ src/main/resources/db/migration/V1_creat_table.sql
@@ -1,7 +1,9 @@
+create database banking_app;
+
 create table accounts
 (
-    id                  bigint auto_increment
-        primary key,
+    id bigint auto_increment,
     account_holder_name varchar(255) not null,
-    balance             bigint       not null
+    balance             bigint       not null,
+	primary key (id)
 );
