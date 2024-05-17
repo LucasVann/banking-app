709807d6fd06b202a75b42ce581efac383a05755
Lucas <vannucchilucas8@gmail.com>
2024-05-17 15:05:01 -0300

    Se agrego un banner

--- src/main/java/ar/com/lucas/bankingapp/BankingAppApplication.java
+++ src/main/java/ar/com/lucas/bankingapp/BankingAppApplication.java
@@ -2,13 +2,13 @@ package ar.com.lucas.bankingapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class BankingAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankingAppApplication.class, args);
+
 	}
 
 }
